c2d1bee726378dc10b411292342782b474389129
Jay Ullman <jayullman@gmail.com>
2017-05-08 22:19:20 -0700

    Add database connection

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+
 
 const port = process.env.PORT || 3000;
+
+require('dotenv').config();
+
+// connect to database
+const url = process.env.MONGOLAB_URI;
+const db = mongoose.connection;
+const options = {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+mongoose.connect(url, options);
+db.on('open', () => {
+  console.log('Connected to databse');
+});
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
